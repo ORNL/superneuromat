46e73e5bc24259000528017c420b7bd0c7bf25cc
kenblu24 <kenblu24@hotmail.com>
2025-04-16 11:20:00 -0400

    Revert "use bool instead of np.bool for wider numpy compatibility"
    This reverts commit d4bd646037c16342b78a0f5e9e9ccb9a57d1363d.

--- src/superneuromat/numba_jit.py
+++ src/superneuromat/numba_jit.py
@@ -47,7 +47,7 @@ def lif_jit(
     refractory_periods[indices] -= 1
 
     # For spiking neurons, turn on refractory period
-    mask = spikes.astype(bool)
+    mask = spikes.astype(np.bool)
     refractory_periods[mask] = refractory_periods_original[mask]
 
     # Reset internal states (in-place)
